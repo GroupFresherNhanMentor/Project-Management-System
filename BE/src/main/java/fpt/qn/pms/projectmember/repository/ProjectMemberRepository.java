187effcd395b5ec13845061a801ce9fa0803f11a
TrongNghia <ntnghia256@gmail.com>
2026-07-21 09:46:44 +0700

    feat: Add security to check isPM, isMember, and permission access with specific task

--- BE/src/main/java/fpt/qn/pms/projectmember/repository/ProjectMemberRepository.java
+++ BE/src/main/java/fpt/qn/pms/projectmember/repository/ProjectMemberRepository.java
@@ -1,7 +1,11 @@
 package fpt.qn.pms.projectmember.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import fpt.qn.pms.common.repository.Repository;
 import fpt.qn.pms.jooq.tables.records.ProjectMembersRecord;
 
 public interface ProjectMemberRepository extends Repository<ProjectMembersRecord> {
+    Optional<ProjectMembersRecord> findByProjectIdAndUserId(UUID projectId, UUID userId);
 }
