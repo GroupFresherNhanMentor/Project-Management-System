708eafa2f5d885da46c984d1166ff122d23fcbb7
Senior Son The <buitheson2004@gmail.com>
2026-07-22 17:23:52 +0700

    fix: add /projects to SSR Server mode to avoid prerender API timeout

--- FE/src/app/app.routes.server.ts
+++ FE/src/app/app.routes.server.ts
@@ -1,6 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'projects',                  renderMode: RenderMode.Server },
   { path: 'projects/new',              renderMode: RenderMode.Server },
   { path: 'projects/:id',              renderMode: RenderMode.Server },
   { path: 'projects/:id/edit',         renderMode: RenderMode.Server },
