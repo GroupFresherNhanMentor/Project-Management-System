1bef7ebbf505e9653dfdeb7d76bea237b46b5b8d
kaine45-cyber <locphan8541@gmail.com>
2026-07-22 16:40:01 +0700

    feat(project): guard project and member views

--- FE/src/app/features/members/members.routes.ts
+++ FE/src/app/features/members/members.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
+import { projectViewGuard } from '../../core/guards/project-view-guard';
 
 export const MEMBERS_ROUTES: Routes = [
   {
     path: '',
+    canActivate: [projectViewGuard],
     loadComponent: () => import('./pages/member-list/member-list').then(m => m.MemberList),
     data: { title: 'Members' },
   },
   {
     path: 'new',
+    canActivate: [projectViewGuard],
     loadComponent: () => import('./pages/member-new/member-new').then(m => m.MemberNew),
     data: { title: 'Add Member' },
   },
